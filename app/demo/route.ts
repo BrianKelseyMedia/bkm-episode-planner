2e6c0898941d6e023840c5989caa0cd949a245c2
BrianKelseyMedia <brian@briankelsey.com>
2026-02-15 20:51:23 -0500

    Update route.ts

--- app/demo/route.ts
+++ app/demo/route.ts
@@ -1,1 +1,14 @@
+import { NextResponse } from "next/server";
 
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+
+  const res = NextResponse.redirect(new URL("/planner", url.origin));
+
+  res.cookies.set("bkm_paid", "0", {
+    path: "/",
+    maxAge: 0,
+  });
+
+  return res;
+}
